dd3c08a4f96ac2621a7a5a99380ce26ba1c29dbb
M3DxM3D <127242136+M3DxM3D@users.noreply.github.com>
2023-03-28 16:29:45 +0200

    bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+    return (
+
+        <div>
+            <figure>
+                <img src="./src/assets/bulbizare.png" alt="bulbasaur"/>
+            </figure>
+
+            <figcaption>bulbasaur</figcaption>
+        </div>
+
+    );
+};
+
+export default PokemonCard;
