e7390e4d5eff07ea03fb044de87de52efc04b3a3
M3DxM3D <127242136+M3DxM3D@users.noreply.github.com>
2023-03-28 22:44:43 +0200

    compo 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,14 +2,41 @@ function PokemonCard() {
     return (
 
         <div>
-            <figure>
-                <img src="./src/assets/bulbizare.png" alt="bulbasaur"/>
+            <figure className="card">
+                <img src="./src/assets/bulbizare.png" alt="bulbasaur" className="card-img"/>
             </figure>
 
             <figcaption>bulbasaur</figcaption>
         </div>
 
+
+const pokemon = pokemonList[1];
+    return (
+        <div> {
+            pokemon.image ? <img src={
+                    pokemon.image
+                }
+                alt={
+                    pokemon.name
+                }/> : <p>???</p>
+        } </div>
     );
+
+    );
+
+
 };
+
+const pokemonList = [
+
+
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    }, {
+        name: "mew"
+    },
+];
+
 
 export default PokemonCard;
